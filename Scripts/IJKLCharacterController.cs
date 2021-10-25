f0a271eda1dd8b8250d9bc1a61d5b25313d75a97
gabriel garcia jaubert <alu0101240374@ull.edu.es>
2021-10-25 23:59:58 +0100

    ultimos fallos

--- Scripts/IJKLCharacterController.cs
+++ Scripts/IJKLCharacterController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class IJKLCharacterController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 100f;
     private Rigidbody rigid;
     void Start()
     {
